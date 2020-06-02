f719ef88ae103fc4db57b94516e5c8abf5066835
Aidan Garton <amgb2018@mymail.pomona.edu>
2020-06-01 22:03:37 -0500

    Implemented authentication on back end... still need to add auth form and hook it up

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
+const crypto = require("crypto");
 
 const User = db.define("user", {
   email: {
@@ -18,6 +19,47 @@ const User = db.define("user", {
       return () => this.getDataValue("password");
     },
   },
+  salt: {
+    type: Sequelize.STRING,
+
+    get() {
+      return () => this.getDataValue("salt");
+    },
+  },
 });
 
 module.exports = User;
+
+// INSTANCE METHODS
+
+User.prototype.correctPassword = function (candidatePwd) {
+  return User.encryptPassword(candidatePwd, this.salt()) === this.password();
+};
+
+// CLASS METHODS
+
+User.generateSalt = function () {
+  return crypto.randomBytes(16).toString("base64");
+};
+
+User.encryptPassword = function (plainText, salt) {
+  return crypto
+    .createHash("RSA-SHA256")
+    .update(plainText)
+    .update(salt)
+    .digest("hex");
+};
+
+// HOOKS
+const setSaltAndPassword = user => {
+  if (user.changed("password")) {
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password(), user.salt());
+  }
+};
+
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
+User.beforeBulkCreate(users => {
+  users.forEach(setSaltAndPassword);
+});
--- server/index.js
+++ server/index.js
@@ -7,6 +7,25 @@ const morgan = require("morgan");
 const PORT = process.env.PORT || 3000;
 const db = require("./db");
 
+const session = require("express-session");
+
+// configure and create our database (session) store
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sessionStore = new SequelizeStore({ db });
+const passport = require("passport");
+
+// passport registration
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.user.findByPk(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 // serve up static files
 app.use(express.static(path.join(__dirname, "../public")));
 
@@ -19,6 +38,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // matches all requests to /api
 app.use("/api", require("./api"));
+
+// session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "a wildly insecure secret",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // serve index.html to all non-matching routes
 app.get("*", function (req, res) {
